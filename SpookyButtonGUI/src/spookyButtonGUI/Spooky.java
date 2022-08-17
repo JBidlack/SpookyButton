8ae221328f8e9168a9efb40f676cc01ebc536338
John Bidlack <82181084+JBidlack@users.noreply.github.com>
2022-08-17 17:41:36 -0400

    Add files via upload

--- SpookyButtonGUI/src/spookyButtonGUI/Spooky.java
+++ SpookyButtonGUI/src/spookyButtonGUI/Spooky.java
@@ -2,6 +2,9 @@ package spookyButtonGUI;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -20,21 +23,41 @@ public class Spooky extends JFrame{
 	public Spooky() {
 		
 		ImageIcon skull = new ImageIcon("skull.png");
-		JLabel skullLabel = new JLabel(skull);
-		JFrame window= new JFrame("It's nearly October");
+		Image icon = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB_PRE);
+		JFrame window= new JFrame("It's Nearly October");
 		JPanel panel = new JPanel();
+		JPanel graphicsPanel = new JPanel();
+		JPanel buttonPanel = new JPanel();
+		JLabel skullLabel = new JLabel(skull);
 		JLabel time = new JLabel("Time to get spooky!");
-		JButton distance = new JButton("Yes");
-		JButton temp = new JButton("Hell Yes");
+		JButton yes = new JButton("Yes");
+		JButton hellYes = new JButton("Hell Yes");
 		
 		window.setPreferredSize(SIZE);
+		panel.setPreferredSize(SIZE);
+		graphicsPanel.setPreferredSize(new Dimension(400, 87));
+		buttonPanel.setPreferredSize(new Dimension(400, 97));
+		yes.setPreferredSize(new Dimension(100, 30));
+		hellYes.setPreferredSize(new Dimension(100, 30));
+		
+		window.setIconImage(icon);
 		window.setResizable(false);
 		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		window.setFont(new Font("Hellvetica", Font.PLAIN, 16));
 		
-		panel.setPreferredSize(SIZE);
+		time.setFont(new Font("Hellvetica", Font.PLAIN, 14));
+		
+		graphicsPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 0));
+		graphicsPanel.add(skullLabel);
+		graphicsPanel.add(time);
+		
+		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 0));
+		buttonPanel.add(yes);
+		buttonPanel.add(hellYes);
+		
 		panel.setLayout(new FlowLayout());
-		
-		panel.add(skullLabel);
+		panel.add(graphicsPanel);
+		panel.add(buttonPanel);
 		
 		window.add(panel);
 		window.pack();
---
+++ SpookyButtonGUI/src/spookyButtonGUI/Yes.java
@@ -0,0 +1,5 @@
+package spookyButtonGUI;
+
+public class Yes {
+
+}
